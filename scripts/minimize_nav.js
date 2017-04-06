ceab66df6fa4977ec023ec15ec9fa3014cf44e72
Pashan <pashanirani@gmail.com>
2017-04-06 15:24:11 -0700

    more minor changes

--- scripts/minimize_nav.js
+++ scripts/minimize_nav.js
@@ -5,7 +5,6 @@ $(function() {
     $(document).scroll(function() {
         if ($(document).scrollTop() > 100 && !scaledNav) {
             $(".nav").animate({
-                padding: 10,
                 "font-size": "25px"
             }, 500, function() {
               scaledNav = true;

0f131bab2cbff6f52a636c94f0f75e5a66605c43
Pashan <pashanirani@gmail.com>
2017-04-07 18:54:12 -0700

    made it more sleek

--- scripts/minimize_nav.js
+++ scripts/minimize_nav.js
@@ -1,20 +1,56 @@
 var nav_visible = false;
-var animation_speed = 500;
+var animation_speed = 250;
+var fancy_animation_speed = 500;
 var nav_opacity = 0.8;
 $(function() {
     $(document).scroll(function() {
-        if ($(document).scrollTop() > 500) {
+        if ($(document).scrollTop() > 200) {
             if (!nav_visible) {
                 showNav();
+                hideElement(".title");
             }
-        } else {
-            if (nav_visible) {
-                hideNav();
-            }
+        } else if (nav_visible) {
+            hideNav();
+            showElement(".title");
         }
 
+
+        showWhenInPos(".testPara", 400);
     });
 });
+
+function showWhenInPos(element, pos) {
+    if (scrollTopRelative(element) > pos) {
+        $(element).animate({
+            opacity: 1
+        }, fancy_animation_speed);
+    }
+}
+
+function hideWhenInPos(element, pos) {
+    if (scrollTopRelative(element) > pos) {
+        $(element).animate({
+            opacity: 0
+        }, fancy_animation_speed);
+    }
+}
+
+function hideWhenBetween(element, start, end) {
+    if (scrollTopRelative(element) > start && scrollTopRelative(element) < end) {
+        $(element).animate({
+            opacity: 0
+        }, fancy_animation_speed);
+    } else {
+        $(element).animate({
+            opacity: 1
+        }, fancy_animation_speed);
+    }
+}
+
+
+function scrollTopRelative(element) {
+    return -1 * ($(element).scrollTop() - $(document).scrollTop());
+}
 
 function showNav() {
     nav_visible = true;
@@ -27,7 +63,19 @@ function showNav() {
 function hideNav() {
     nav_visible = false;
     $(".nav").animate({
-        "font-size": "25px",
+        "font-size": "28px",
         opacity: 0
+    }, animation_speed);
+}
+
+function hideElement(element) {
+    $(element).animate({
+        opacity: 0
+    }, animation_speed);
+}
+
+function showElement(element) {
+    $(element).animate({
+        opacity: 1
     }, animation_speed);
 }
